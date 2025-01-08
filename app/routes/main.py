13a2a3795745da0c8d21b107d434aa0b9aa6b904
乱世羁绊 <122093948+luanshijiban@users.noreply.github.com>
2025-01-08 12:30:58 +0800

    Delete app directory

--- app/routes/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-from app.models.product import Product
-
-main_bp = Blueprint('main', __name__)
-
-@main_bp.route('/')
-def index():
-    return render_template('index.html')  # 直接使用根目录下的 index.html
